ef15f1279e0c041501e856712397ad6ca9e996de
rizhenkolv <90309520+rizhenkolv@users.noreply.github.com>
2023-08-17 13:02:46 +0200

    The pet project

---
+++ xiaomi/js/main.js
@@ -0,0 +1,27 @@
+
+$(function(){
+
+    $('.bike-slider').slick({
+        arrows: false,
+        dots: true,
+        fade:true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+
+    });
+    $('.slider__items').slick({
+        arrows: false,
+        dots: true,
+        fade:true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+
+    });
+
+
+    
+
+})
+
+
+
